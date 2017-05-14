e57081c28a7afd74a93cfb9d97a58113fe69b5b4
Isabel Ramos <iramos1226@gmail.com>
2017-05-14 18:04:09 -0500

    print to dom and filter text works

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,4 +2,38 @@ app.run((FIREBASE_CONFIG) => {
   firebase.initializeApp(FIREBASE_CONFIG);
 });
 
-console.log("hellooo.");
+app.controller("MushroomCtrl", ($http, $q, $scope, FIREBASE_CONFIG) => {
+	// $scope.showListView = true;
+	$scope.mushrooms = [];
+
+
+  let getMushroomList = () => {
+    let mushroomz = [];
+    return $q ((resolve, reject) => {
+      $http.get(`${FIREBASE_CONFIG.databaseURL}/mushrooms.json`)
+      .then((firebaseItems) => {
+          let itemCollection = firebaseItems.data;
+          Object.keys(itemCollection).forEach((key) => {
+            itemCollection[key].id = key;
+            mushroomz.push(itemCollection[key]);
+          });
+          resolve(mushroomz);
+      }).catch((error) => {
+        reject(error);
+      });
+    }); 
+  };
+
+
+  let getMushrooms = () => {
+    getMushroomList().then((mushroomz) => {
+      $scope.mushrooms = mushroomz;
+    }).catch((error) => {
+      console.log("error", error);
+    });
+  };
+
+
+  getMushrooms();
+  
+});
