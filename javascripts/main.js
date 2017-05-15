f34efd5f669eec1b6b1e59dde6dcb0b53a690eec
Isabel Ramos <iramos1226@gmail.com>
2017-05-14 19:18:03 -0500

    filtering with radio buttons works

--- javascripts/main.js
+++ javascripts/main.js
@@ -7,33 +7,33 @@ app.controller("MushroomCtrl", ($http, $q, $scope, FIREBASE_CONFIG) => {
 	$scope.mushrooms = [];
 
 
-  let getMushroomList = () => {
-    let mushroomz = [];
-    return $q ((resolve, reject) => {
-      $http.get(`${FIREBASE_CONFIG.databaseURL}/mushrooms.json`)
-      .then((firebaseItems) => {
-          let itemCollection = firebaseItems.data;
-          Object.keys(itemCollection).forEach((key) => {
-            itemCollection[key].id = key;
-            mushroomz.push(itemCollection[key]);
-          });
-          resolve(mushroomz);
-      }).catch((error) => {
-        reject(error);
-      });
-    }); 
-  };
+	let getMushroomList = () => {
+		let mushroomz = [];
+		return $q ((resolve, reject) => {
+		$http.get(`${FIREBASE_CONFIG.databaseURL}/mushrooms.json`)
+		.then((firebaseItems) => {
+			let itemCollection = firebaseItems.data;
+			Object.keys(itemCollection).forEach((key) => {
+            	itemCollection[key].id = key;
+            	mushroomz.push(itemCollection[key]);
+          	});
+          	resolve(mushroomz);
+	      	}).catch((error) => {
+	        	reject(error);
+	    	});
+    	}); 
+  	};
 
 
-  let getMushrooms = () => {
-    getMushroomList().then((mushroomz) => {
-      $scope.mushrooms = mushroomz;
-    }).catch((error) => {
-      console.log("error", error);
-    });
-  };
+	let getMushrooms = () => {
+		getMushroomList().then((mushroomz) => {
+    		$scope.mushrooms = mushroomz;
+    	}).catch((error) => {
+      	console.log("error", error);
+    	});
+  	};
 
 
-  getMushrooms();
-  
+	getMushrooms();
+
 });
